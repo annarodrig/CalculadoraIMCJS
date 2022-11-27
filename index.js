025e93042906dac775be334ab9fb4ddfef311192
Anna Beatriz <annab_oliver@hotmail.com>
2022-11-27 17:59:56 -0300

    Corrigi bugs

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ let alturaUsuario = null;
 let botaoCalcula = null;
 let paragrafoResultado = null;
 let exibeIMC = null;
-let exibeSituaco = null;
+let exibeSituacao = null;
 
 // Armazena os dados do usuario neste objeto
 let dadosUsuario = {
@@ -13,10 +13,11 @@ let dadosUsuario = {
     massa: null
 }
 
+
 // Evento que ocorre quando o DOM for carregado 
 document.addEventListener("DOMContentLoaded", iniciar);
 
-const iniciar = () => {
+function iniciar() {
 
     // Obtendo os campos necessários do DOM
     pesoUsuario = document.getElementById("peso");
@@ -24,61 +25,59 @@ const iniciar = () => {
     botaoCalcula = document.getElementById("calcula");
     paragrafoResultado = document.getElementById("divCalculos");
     exibeIMC = document.getElementById("resultadoIMC");
-    exibeSituaco = document.getElementById("resultadoSituacao");
+    exibeSituacao = document.getElementById("resultadoSituacao");
 
     // Armazenando os valores nas propriedades de "dadosUsuario" quando os campos mudam
     pesoUsuario.addEventListener("change", () => dadosUsuario.peso = Number(pesoUsuario.value));
 
     alturaUsuario.addEventListener("change", () => dadosUsuario.altura = Number(alturaUsuario.value));
 
-        // Cria função que testa se o verifica a situação que o usuário esta de acordo com seu IMC
-
-        function testaSituacao(valorIMC) {
-            let imc = Number(valorIMC);
-    
-            if (imc < 17)
-            {
-                return("Diagnóstico: Muito abaixo do peso" );
-            }
-            else if (imc < 18.50 )
-            {
-                return("Diagnóstico: Abaixo do peso" );
-            }
-            else if ( imc < 25)
-            {
-                return("Diagnóstico: Peso normal" );
-            }
-            else if (imc < 30)
-            {
-                return("Diagnóstico: Acima do peso" );
-            }
-            else if (imc < 35)
-            {
-                return("Diagnóstico: Obesidade |" );
-            }
-            else if (imc < 40)
-            {   
-                return("Diagnóstico: Obesidade ||(severa)" );
-            }
-            else if (imc > 40)
-            {
-                return("Diagnóstico: Obesidade |||(mórbida)" );
-            }
-    
-    }
-
     // Cria função que calcula o IMC do usuário quando ele clicar no button e exibe os resultados do "paragrafoResultado"
 
     botaoCalcula.addEventListener("click", () => {
-        if (dadosUsuario.altura != 0 && dadosUsuario.peso != 0){
-            const valorIMC = (dadosUsuario.peso / (dadosUsuario.altura*dadosUsuario.altura));
+        if (dadosUsuario.altura != 0 && dadosUsuario.peso != 0) {
+            
+            const valorIMC = (dadosUsuario.peso / (dadosUsuario.altura ** 2)).toFixed(2);
             const situacaoUsuario = testaSituacao(valorIMC);
 
-            paragrafoResultado.innerText = `${valorIMC} kg/m².`;
-            paragrafoResultado.innerText = `${situacaoUsuario}`;
-            divCalculos.hidden = 0;
-        } else {
-            alert("Insira valores válidos!");
+            exibeIMC.innerText = `${valorIMC} kg/m².`;
+            exibeSituacao.innerText = `${situacaoUsuario}`;
+            paragrafoResultado.hidden = 0;
         }
     });
 }
+
+// Cria função que testa se o verifica a situação que o usuário esta de acordo com seu IMC
+
+function testaSituacao(valorIMC) {
+
+    let IMC = Number(valorIMC)
+
+    switch (true) {
+        case (IMC < 17):
+            return "Muito abaixo do peso.";
+
+        case (IMC < 18.5):
+            return "Abaixo do peso."
+
+        case (IMC < 25):
+            return "Peso normal.";
+
+        case (IMC < 30):
+            return "Acima do peso.";
+
+        case (IMC < 35):
+            return "Obesidade.";
+
+        case (IMC < 40):
+            return "Obesidade severa.";
+
+        case (IMC >= 40):
+            return "Obesidade mórbida.";
+
+        default:
+            return "Um erro ocorreu.";
+    }
+
+}
+
