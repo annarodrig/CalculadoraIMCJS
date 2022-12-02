a616b4ee155817fa1aef4e2045d79091fe4fbd11
Anna Beatriz <annab_oliver@hotmail.com>
2022-12-01 22:15:17 -0300

    Ajusta hidden e evt no index.js

--- index.js
+++ index.js
@@ -3,16 +3,15 @@
 let pesoUsuario = null;
 let alturaUsuario = null;
 let botaoCalcula = null;
-let paragrafoResultado = null;
 let exibeIMC = null;
 let exibeSituacao = null;
+let paragrafoResultado = null;
 
 // Armazena os dados do usuario neste objeto
 let dadosUsuario = {
     altura: null,
-    massa: null
+    peso: null
 }
-
 
 // Evento que ocorre quando o DOM for carregado 
 document.addEventListener("DOMContentLoaded", iniciar);
@@ -28,56 +27,58 @@ function iniciar() {
     exibeSituacao = document.getElementById("resultadoSituacao");
 
     // Armazenando os valores nas propriedades de "dadosUsuario" quando os campos mudam
-    pesoUsuario.addEventListener("change", () => dadosUsuario.peso = Number(pesoUsuario.value));
+    pesoUsuario.addEventListener("change", () => dadosUsuario.peso = parseInt(pesoUsuario.value));
 
-    alturaUsuario.addEventListener("change", () => dadosUsuario.altura = Number(alturaUsuario.value));
+    alturaUsuario.addEventListener("change", () => dadosUsuario.altura = parseFloat(alturaUsuario.value));
 
     // Cria função que calcula o IMC do usuário quando ele clicar no button e exibe os resultados do "paragrafoResultado"
 
-    botaoCalcula.addEventListener("click", () => {
+    botaoCalcula.addEventListener("click", (evt) => {
+        evt.preventDefault();
         if (dadosUsuario.altura != 0 && dadosUsuario.peso != 0) {
             
-            const valorIMC = (dadosUsuario.peso / (dadosUsuario.altura ** 2)).toFixed(2);
+            const valorIMC = (dadosUsuario.peso / (dadosUsuario.altura.toFixed(2) ** 2)).toFixed(2);
             const situacaoUsuario = testaSituacao(valorIMC);
 
             exibeIMC.innerText = `${valorIMC} kg/m².`;
             exibeSituacao.innerText = `${situacaoUsuario}`;
-            paragrafoResultado.hidden = 0;
+            paragrafoResultado.removeAttribute('hidden');
+    
         }
     });
-}
 
-// Cria função que testa se o verifica a situação que o usuário esta de acordo com seu IMC
+    // Cria função que testa se o verifica a situação que o usuário esta de acordo com seu IMC
 
-function testaSituacao(valorIMC) {
+    function testaSituacao(valorIMC) {
 
-    let IMC = Number(valorIMC)
+        let IMC = Number(valorIMC);
 
-    switch (true) {
-        case (IMC < 17):
-            return "Muito abaixo do peso.";
+        switch (true) {
+            case (IMC < 17):
+                return "Muito abaixo do peso.";
 
-        case (IMC < 18.5):
-            return "Abaixo do peso."
+            case (IMC < 18.5 ):
+                return "Abaixo do peso."
 
-        case (IMC < 25):
-            return "Peso normal.";
+            case (IMC < 25):
+                return "Peso normal.";
 
-        case (IMC < 30):
-            return "Acima do peso.";
+            case (IMC < 30):
+                return "Acima do peso.";
 
-        case (IMC < 35):
-            return "Obesidade.";
+            case (IMC < 35):
+                return "Obesidade.";
 
-        case (IMC < 40):
-            return "Obesidade severa.";
+            case (IMC < 40):
+                return "Obesidade severa.";
 
-        case (IMC >= 40):
-            return "Obesidade mórbida.";
+            case (IMC >= 40):
+                return "Obesidade mórbida.";
 
-        default:
-            return "Um erro ocorreu.";
+            default:
+                return "Um erro ocorreu.";
+        }
+
     }
 
 }
-
